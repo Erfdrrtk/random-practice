6ab6486eae81b24d583a8f0da6c5acc59920b52c
Erfdrrtk <emmanuel937589@gmail.com>
2022-01-09 13:50:04 -0600

    holy shit fucntiins are so similair to function in math lolkl

--- 1.py
+++ 1.py
@@ -1,3 +1,17 @@
-course = 'python for beginners'
+def f(x):
+    print(x ** 2 + 1)
+f(2)
 
-print(course.find(' '))
+def buildings(owner, founder):
+    print(f'the owner of apple of apple is {owner}. the founder of apple is {founder}')
+
+buildings('Emmanuel', 'David')
+
+
+def f(a, add_three_to_a):
+    a = a + add_three_to_a
+    print(a ** 2 + 1)
+f(5, 3)
+
+
+
