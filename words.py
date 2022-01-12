1d45f45783aac42439d47bcdde4fbe8811e25cd0
Erfdrrtk <emmanuel937589@gmail.com>
2022-01-12 00:27:20 -0600

    messed around with for loops and lists

--- words.py
+++ words.py
@@ -1,9 +1,15 @@
-rog = [1, 33, 53, 120 ,50 ,6034]
-compare = 0
-
-for number in rog:
-    if number > compare:
-        compare = number
-    print(compare, number)
-# once the loop is done, the biggest number will be printed
-print(f'the biggest number is {compare}')
+count = 0
+print('count   number')  # 3 spaces
+list = [1, 22, 44, 55, 64, 15, 4]
+for number in list:
+    count = count + 1
+    print(f'{count}       {number}') # 7 spaces
+# these are more simple to understand:
+biggest = max(list)
+smaller = min(list)
+sum = sum(list)
+average = sum/count
+print(f'the biggest number of this list is {biggest}')
+print(f'the smallest number of this list is {smaller}')
+print(f'the sum of this list is {sum}')
+print(f'the average of this list is: {int(average)}')
